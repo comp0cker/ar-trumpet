3fe98e79129e891bd7c32264cd3e6b2c716671a3
Jared Grimes <jaredlgrimes@hotmail.com>
2020-02-06 13:37:55 -0500

    cube works

--- ARTrumpet.playground/Contents.swift
+++ ARTrumpet.playground/Contents.swift
@@ -12,15 +12,28 @@ extension float4x4 {
 
 public class LiveVC: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
     let scene = SCNScene()
+    //public var arscn = ARSCNView()
     public var arscn = ARSCNView(frame: CGRect(x: 0,y: 0,width: 640,height: 360))
     
-    var ship: SCNNode!
+    var cubeNode: SCNNode!
     // var firstValve: SCNNode!
     // var secondValve: SCNNode!
     // var thirdValve: SCNNode!
     
     override public func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(arscn)
+        
+        setupSceneView()
+        // addCoachingOverlay()
+        addTapGestureToSceneView()
+    }
+    
+    func setupSceneView() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        arscn.session.run(config)
         
         arscn.delegate = self
         arscn.session.delegate = self
@@ -34,50 +47,72 @@ public class LiveVC: UIViewController, ARSessionDelegate, ARSCNViewDelegate {
                                    .showCameras*/
                                  ]
         //arscn.showsStatistics = true
-        arscn.session.delegate = self
+    }
         
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal]
-        arscn.session.run(config)
-        
-        view.addSubview(arscn)
-        
-        addCoachingOverlay()
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap(_:)))
-        arscn.addGestureRecognizer(tapGesture)
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: arscn, action: #selector(handleTap(rec:)))
+        arscn.addGestureRecognizer(tapGestureRecognizer)
     }
     
-    @objc func didTap(_ gesture: UIPanGestureRecognizer) {
-        // Ensure ship is on screen
-        guard let _ = ship else { return }
-
-        let tapLocation = gesture.location(in: arscn)
-        let results = arscn.hitTest(tapLocation, types: .featurePoint)
-
-        if let result = results.first {
-            let translation = result.worldTransform.translation
-            ship.position = SCNVector3Make(translation.x, translation.y, translation.z)
-            arscn.scene.rootNode.addChildNode(ship)
-        }
+    // when you tap ON THE SCREEN
+    @objc func handleTap(rec: UIGestureRecognizer) {
+        let alert = UIAlertController(title: "This is a trumpet.", message: "Hopefully?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        vc.present(alert, animated: true, completion: nil)
     }
     
+    // initially render the plane
     public func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        let shipScn = SCNScene(named: "ship.scn", inDirectory: "art.scnassets")
 
-        ship = shipScn?.rootNode
-        ship.simdPosition = SIMD3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        // Create a SceneKit plane to visualize the node using its position and extent.
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        // Makes the plane clear blue
+        plane.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.5)
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
 
-        arscn.scene.rootNode.addChildNode(ship)
-        node.addChildNode(ship)
+        // We rotate the plane to match the surface (instead of the plane standing upright)
+        planeNode.eulerAngles.x = -.pi / 2
+
+        // ARKit owns the node corresponding to the anchor, so make the plane a child node.
+        node.addChildNode(planeNode)
+        
+        // this is the trumpet we add?
+        cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        //cubeNode.position = SCNVector3(-1, -1, 0) // SceneKit/AR coordinates are in meters
+        node.addChildNode(cubeNode)
+    }
+    
+    // Update the plane as we move around
+    public func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+        let planeNode = node.childNodes.first,
+        let plane = planeNode.geometry as? SCNPlane
+        else { return }
+
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
     }
     
     func addCoachingOverlay() {
         let coachingOverlay = ARCoachingOverlayView()
+        /*
         coachingOverlay.autoresizingMask = [
           .flexibleWidth, .flexibleHeight
         ]
+ */
         coachingOverlay.goal = .horizontalPlane
         coachingOverlay.session = arscn.session
         
--- ARTrumpet.playground/Sources/Note.swift
+++ ARTrumpet.playground/Sources/Note.swift
@@ -1,4 +1,5 @@
 import Foundation
+import AVFoundation
 
 // All enharmonics are written as note flat
 enum Pitch: Int {
@@ -17,23 +18,35 @@ enum Pitch: Int {
 }
 
 /**
-Intervals are defined corresponding to the harmonics on a trumpet. In the open position, the following ints produce the following tones:
-0: C4
-1: G4
-2: C5
-3: E5
-4: G5
-5: Bb5
-6: C6
-7: D6
-8: E6
-9: Gb6
-10: G6
+    Intervals are defined corresponding to the harmonics on a trumpet. In the open position, the following ints produce the following tones:
+    0: C4
+    1: G4
+    2: C5
+    3: E5
+    4: G5
+    5: Bb5
+    6: C6
+    7: D6
+    8: E6
+    9: Gb6
+    10: G6
 */
 
 class Note {
     var pitch: Pitch = .C
     var octave: Int = 5
+    
+    var noteMidSound: AVAudioPlayer?
+    
+    /**
+        Each note is defined as 3 mp3 files in the Resources directory. For the note C4, these are defined as follows:
+        
+        C4_start.mp3
+        C4_mid.mp3
+        C4_end.mp3
+     
+        The start mp3 is played immediately on button press. The mid mp3 is looped indefinitely until the button is released, which initiates the end mp3.
+     */
     
     init(valveOnePressed: Bool, valveTwoPressed: Bool, valveThreePressed: Bool, interval: Int) {
         if interval < 2 {
@@ -57,6 +70,47 @@ class Note {
         }
         
         pitchShift(pitchShift: valvesToPitchShift(valveOnePressed: valveOnePressed, valveTwoPressed: valveTwoPressed, valveThreePressed: valveThreePressed))
+    }
+    
+    func playStart() {
+        let fileName = "\(pitch)\(octave)_start.mp3"
+        play(fileName: fileName)
+    }
+    
+    func playMid() {
+        let fileName = "\(pitch)\(octave).mp3"
+        let path = Bundle.main.path(forResource: "\(fileName)", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            noteMidSound = try AVAudioPlayer(contentsOf: url)
+            noteMidSound?.numberOfLoops = -1
+            noteMidSound?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+    
+    func stopMid() {
+        noteMidSound?.stop()
+    }
+    
+    func playEnd() {
+        let fileName = "\(pitch)\(octave)_end.mp3"
+        play(fileName: fileName)
+    }
+    
+    func play(fileName: String) {
+        var noteSound: AVAudioPlayer?
+        let path = Bundle.main.path(forResource: "\(fileName)", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            noteSound = try AVAudioPlayer(contentsOf: url)
+            noteSound?.play()
+        } catch {
+            // couldn't load file :(
+        }
     }
     
     func pitchShift(pitchShift: Int) {
